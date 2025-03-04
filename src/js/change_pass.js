c42d6ac97a855e07cdbbebfed922bd592504523d
Demetrius Ivankov <papilovdim@yandex.ru>
2025-03-04 12:05:03 +0300

    add: inputs change pass

--- src/js/change_pass.js
+++ src/js/change_pass.js
@@ -3,19 +3,15 @@ import '../blocks/#common/common.styles.scss';
 /* шапка */
 import '../blocks/header/header.scss';
 import '../blocks/header/header.js';
-/* боковая панель */
-import '../blocks/sidebar/sidebar.scss';
-import '../blocks/sidebar/sidebar.js';
 /* кнопки */
 import '../blocks/button/__control/button__control.scss';
 import '../blocks/button/__control/_modal/button__control_modal.scss';
-import '../blocks/button/__control/button__control.js';
 import '../blocks/button/__tab/button__tab.scss';
 import '../blocks/button/__tab/button__tab.js';
 import '../blocks/button/__select/button__select.scss'
 /* input */
 import '../blocks/input/__type-text/input__type-text.scss';
-//import '../blocks/input/__type-text/_modal/input__type-text_modal.scss';
+import '../blocks/input/__type-text/_modal/input__type-text_modal.scss';
 //import '../blocks/input/__type-checkbox/input__type-checkbox.scss';
 import '../blocks/input/__type-date/input__type-date.scss';
 import '../blocks/input/__type-radio/input__type-radio.scss';
@@ -25,8 +21,6 @@ import '../blocks/select/select.js';
 import '../blocks/select/_modal/select_modal.scss';
 /* модальные окна */
 import '../blocks/modal/modal.scss';
-/* дерево */
-import '../blocks/tree/jstree.scss';
-import '../blocks/tree/jstree.js';
+
 /* таблицы */
 import '../blocks/table/table.scss';
