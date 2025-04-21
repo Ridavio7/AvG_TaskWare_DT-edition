00d052ef347d25a6ee0b363daeb0cb049e79883b
Demetrius Ivankov <papilovdim@yandex.ru>
2025-04-21 11:56:54 +0300

    fix: header

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -4,7 +4,7 @@ function setTheme(themeName) {
     document.documentElement.className = themeName;
 }
 
-let header__user__switch = document.getElementsByClassName("header__user__switch-input");
+let header__user__switch = document.getElementsByClassName("header__user__switch-theme-input");
 header__user__switch[0].addEventListener("click", (e)=>{
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-light');
@@ -23,14 +23,13 @@ header__user__switch[0].addEventListener("click", (e)=>{
     }
 })();
 
-let header_user_arrow = document.getElementsByClassName("header__user__arrow");
-header_user_arrow[0].addEventListener("click", (e)=>{
-    let arrowParent = e.target.parentElement.parentElement;
-    arrowParent.classList.toggle("header__user__container_menu-visibale");
+let header_user_link_not = document.getElementsByClassName("header__user__link_notifications");
+header_user_link_not[0].addEventListener("click", ()=>{
+    window.location = 'notifications.html';
 });
 
 let header_user_link_leave = document.getElementsByClassName("header__user__link_leave-site");
-header_user_link_leave[0].addEventListener("click", (e)=>{
+header_user_link_leave[0].addEventListener("click", ()=>{
     let result = confirm("Вы уверены, что хотите выйти?");
     if(result === true){
         window.location = 'https://dev.proektit.ru';
