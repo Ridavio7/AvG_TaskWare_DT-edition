4fd9f98fa00ca93349430b9ad0e1ee9803e44268
Demetrius Ivankov <papilovdim@yandex.ru>
2025-06-26 15:05:06 +0300

    add: sort shablons

--- src/blocks/table/__template-task-shablons/table__template-task-shablons.js
+++ src/blocks/table/__template-task-shablons/table__template-task-shablons.js
@@ -45,9 +45,14 @@ const funcProcessGetShablons = (result, respobj) => {
     button_modal.forEach((elem) => {
         elem.addEventListener("click", () => {
             let btn = tb_shablons.querySelector('.button__control_active');
-            if(btn != null){btn.classList.remove('button__control_active');}
+            if(btn != null){
+                btn.classList.remove('button__control_active');
+                btn.parentElement.parentElement.classList.remove('tr_mark');
+            }
+
             funcGetShablonsTree(elem.value);
             elem.classList.add('button__control_active');
+            elem.parentElement.parentElement.classList.add('tr_mark');
         })
     })
 
@@ -69,17 +74,13 @@ const addShablonsRow = (name, uin, del, tb_id) => {
     let newRow = tableRef.insertRow(-1);
     newRow.classList = "tr";
 
-    let cellInfo = newRow.insertCell(0); cellInfo.classList = "td";
-    let cellRel  = newRow.insertCell(1); cellRel.classList  = "td";
-    let cellName = newRow.insertCell(2); cellName.classList = "td";
-    let cellBtn  = newRow.insertCell(3); cellBtn.classList  = "td";
+    let cellName = newRow.insertCell(0); cellName.classList = "td";
+    let cellBtn  = newRow.insertCell(1); cellBtn.classList  = "td td__text_align_center";
 
-    cellInfo.innerHTML = `<button class="button__control button__control_modal-shablons-catSh" value="${uin}"><img class="button__control__img" src="assets/images/info.svg" alt=""></button>`;
-    cellRel.innerHTML  = `<button class="button__control button__control_modal-shablons-release" value="${uin}" name="${name}">Запуск</button>`;
-    cellName.innerHTML = name;
+    cellName.innerHTML = `<button class="button__control button__control_action button__control_modal-shablons-catSh" value="${uin}">${name}</button>`;
 
     let bx_color = del === 0 ? bx_color = "" : bx_color = " button__control_mdel_active"; cellBtn.classList = "td td_buttons-control";
-    cellBtn.innerHTML = `<button class="button__control button__control_mdel button__control_mdel-shablons${bx_color}" value="${uin}"><img class="button__control__img" src="assets/images/cross.svg"></button>`;
+    cellBtn.innerHTML = `<button class="button__control button__control_modal-shablons-release" value="${uin}" name="${name}">Запуск</button><button class="button__control button__control_mdel button__control_mdel-shablons${bx_color}" value="${uin}"><img class="button__control__img" src="assets/images/cross.svg"></button>`;
 }
 
 let button_control_add = document.querySelector(".button__control_add-shablons");
@@ -100,7 +101,33 @@ button_control_add.addEventListener("click", () => {
     }
 })
 
-listenSortSelect("sort_shablons", "tb_shablons", "shablons", funcProcessGetShablons);
+document.getElementById("sort_shablons").addEventListener('change', function(){
+    document.getElementById('tb_shablons').innerHTML = '';
+
+    let option = this.selectedIndex;
+    switch (option){
+        case 0:
+        let body0  =  {"user":`${localStorage.getItem('srtf')}`, "meth":"view", "obj":"shablons", "count":"5000"};
+        funcCommand(body0, funcProcessGetShablons);
+        break;
+        case 1:
+        let body1  =  {"user":`${localStorage.getItem('srtf')}`, "meth":"view", "obj":"shablons", "count":"5000", "asort":"name"};
+        funcCommand(body1, funcProcessGetShablons);
+        break;
+        case 2:
+        let body2  =  {"user":`${localStorage.getItem('srtf')}`, "meth":"view", "obj":"shablons", "count":"5000", "sort":"name"};
+        funcCommand(body2, funcProcessGetShablons);
+        break;
+        case 3:
+        let body3  =  {"user":`${localStorage.getItem('srtf')}`, "meth":"view", "obj":"shablons", "count":"5000", "asort":"uin"};
+        funcCommand(body3, funcProcessGetShablons);
+        break;
+        case 4:
+        let body4  =  {"user":`${localStorage.getItem('srtf')}`, "meth":"view", "obj":"shablons", "count":"5000", "sort":"uin"};
+        funcCommand(body4, funcProcessGetShablons);
+        break;
+    }
+})
 
 export const funcGetShablonsTree = (uin) => {
     let body = {"user":`${localStorage.getItem('srtf')}`, "meth":"view", "obj":"catSh", "uinShablon":`${uin}`};
