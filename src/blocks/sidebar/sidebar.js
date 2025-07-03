be47376e18842f5270f1da6c01303dbad8b1b3bd
Demetrius Ivankov <papilovdim@yandex.ru>
2025-07-03 12:10:59 +0300

    fix: divide sidebar js

--- src/blocks/sidebar/sidebar.js
+++ src/blocks/sidebar/sidebar.js
@@ -3,26 +3,38 @@ expand_btn.addEventListener("click", () => {
     document.body.classList.toggle("collapsed");
 });
 
-/* sidebar первые кнопки */
-let sidebar_links = document.querySelectorAll(".sidebar__link");
-sidebar_links.forEach((elem) => {
+const setSidebarEvents = (elem, tabcontent_name, tablinks_name, tablinks_active_name) => {
     elem.addEventListener("click", () => {
         let i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("sidebar__tabcontent");
+        tabcontent = document.getElementsByClassName(tabcontent_name);
         for (i = 0; i < tabcontent.length; i++) {
             tabcontent[i].style.display = "none";
         }
 
-        tablinks = document.getElementsByClassName("sidebar__link");
+        tablinks = document.getElementsByClassName(tablinks_name);
         for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" sidebar__link_active", "");
+            tablinks[i].className = tablinks[i].className.replace(` ${tablinks_active_name}`, "");
         }
         document.getElementById(elem.classList[1]).style.display = "flex";
         document.getElementById(elem.classList[1]).style.flexDirection = "column";
-        elem.className += " sidebar__link_active";
+        elem.className += ` ${tablinks_active_name}`;
 
         if(!document.URL.includes("tasks.html")){
             localStorage.setItem("sidebar_tab_active", elem.id);
         }
-    });
-});
+    })
+}
+
+/* sidebar кнопки */
+let sidebar_links = document.querySelectorAll(".sidebar__link");
+sidebar_links.forEach((elem) => {
+    setSidebarEvents(elem, "sidebar__tabcontent", "sidebar__link", "sidebar__link_active");
+})
+
+/* sidebar кнопки задач */
+setTimeout(() => {
+    let sidebar_links = document.querySelectorAll(".sidebar__link_task");
+    sidebar_links.forEach((elem) => {
+        setSidebarEvents(elem, "sidebar__tabcontent", "sidebar__link_task", "sidebar__link_active");
+    })
+}, 500)
