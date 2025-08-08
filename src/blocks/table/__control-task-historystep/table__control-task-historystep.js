a180db6dd9a1e34dbf7e53aed944a045f335ee0a
Demetrius Ivankov <papilovdim@yandex.ru>
2025-08-08 12:59:17 +0300

    add: table task history step

---
+++ src/blocks/table/__control-task-historystep/table__control-task-historystep.js
@@ -0,0 +1,35 @@
+import {funcCommand, clearTable, formatDate} from '../../../js/common/common.js';
+
+export const funcGetHistorystep = (uin) => {
+    let body  =  { "user":`${localStorage.getItem('srtf')}`, "meth":"view", "obj":"historystep", "uinstep":`${uin}`, "count":"100", "sort":"datetm" };
+    funcCommand(body, funcProcessGetHistorystep);
+}
+
+const funcProcessGetHistorystep = (result, respobj) => {
+    console.log("История шага:", respobj);
+
+    let tb_id = "tb_task_historystep";
+    clearTable(tb_id);
+
+    for (let key in respobj.answ){
+        let obj    = respobj.answ[key];
+        let name   = obj.name;
+        let datetm = obj.datetm;
+        let user   = obj.user.name;
+        let count  = obj.count;
+        let prim   = obj.prim;
+        addHistorystepRow(name, datetm, user, count, prim, tb_id);
+    }
+}
+
+const addHistorystepRow = (name, datetm, user, count, prim, tb_id) => {
+    let tableRef = document.getElementById(tb_id);
+    let newRow = tableRef.insertRow(-1);
+    newRow.classList = "tr";
+
+    let cellUser  = newRow.insertCell(0); cellUser.classList  = "td"; cellUser.innerHTML = user;
+    let cellName  = newRow.insertCell(1); cellName.classList  = "td"; cellName.innerHTML = name;
+    let cellDate  = newRow.insertCell(2); cellDate.classList  = "td"; cellDate.innerHTML = formatDate(datetm);
+    let cellCount = newRow.insertCell(3); cellCount.classList = "td"; cellCount.innerHTML = count;
+    let cellPrim  = newRow.insertCell(4); cellPrim.classList  = "td"; cellPrim.innerHTML = prim;
+}
